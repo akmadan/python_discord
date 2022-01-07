da51cc665aedf0f4ed1fdd04051e758fc5d20ad1
Akshit Madan <akmadan1717@gmail.com>
2022-01-07 14:36:55 +0530

    greet user

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
 import discord 
+import tokens   # secrets is a python file that contains bot token
 client = discord.Client() 
 
-token = 'OTI4OTMxOTkyMTc3ODk3NTQy.Ydf9VQ.-muDF3S8Zk6Mknm5tXaxDTDH9ww'
+token = tokens.TOKEN
+
+@client.event 
+async def on_message(message): 
+    msg = message.content.lower() 
+    if(message.author == client.user):
+        return 
+    else: 
+        
+        if(msg.startswith('hello') or (msg.startswith('hi'))): 
+            await message.channel.send('Hi! '+str(message.author)+'!')
 
 client.run(token)
