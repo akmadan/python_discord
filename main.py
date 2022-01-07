7bdcb3481444d71b2ae634e50de2e1799ec6e59b
Akshit Madan <akmadan1717@gmail.com>
2022-01-07 16:14:25 +0530

    delete message

--- main.py
+++ main.py
@@ -7,11 +7,23 @@ token = tokens.TOKEN
 @client.event 
 async def on_message(message): 
     msg = message.content.lower() 
-    if(message.author == client.user):
+
+
+    if(message.author == client.user):  #no message if bot is only the sender
         return 
     else: 
         
-        if(msg.startswith('hello') or (msg.startswith('hi'))): 
-            await message.channel.send('Hi! '+str(message.author)+'!')
+        ## greet message
+        if(msg.startswith('hello') or (msg.startswith('hi'))):  
+            await message.channel.send('Hi! '+str(message.author)+'!')  #greet back the user
+
+
+        ## delete sensitive message
+        if('kill' in msg):
+            await message.channel.purge(limit = 1)
+
+        
+
+
 
 client.run(token)
